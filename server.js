4f8c9fdffbdb499b80109577ad4428c7d3e8434f
utkarsh380 <patelutkarsh341@gmail.com>
2020-12-28 16:29:05 +0530

    set up express server

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.listen(8000,function(){
+  console.log("server is going to run you can access it on port http://localhost:8000....")
+});
