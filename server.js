0517b0a1fbd0b141d84ef30ae1027c323821aeda
utkarsh380 <patelutkarsh341@gmail.com>
2020-12-28 16:34:57 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+const mockData= [{name:'ajay'},{name:'anjali'}]
+app.gey('/users',function(req,res){
+   res.json({
+       success:true,
+       message:'successfully got users. Nice!',
+       users: mokeUserData
+   })
+})
+
 app.listen(8000,function(){
-  console.log("server is going to run you can access it on port http://localhost:8000....")
-});
+    console.log("server is going to run you can access it on port http://localhost:8000....")
+  });
